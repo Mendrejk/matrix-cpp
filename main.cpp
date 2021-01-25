b53788845654c5fcd041805e0379bb7f831b9b5e
Mendrejk <bastian.lakomy@gmail.com>
2021-01-25 23:04:00 +0100

    Added matrix toString().

--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@ int main() {
     Matrix<int> test_initial_value(2, 1, 0);
     std::vector<std::vector<int>> test_vector =
         {
-            {1, 2, 3},
+            {1, 24781237, 3},
             {4, 5, 6},
-            {7, 8, 9}
+            {7, 8, 9},
+            {9, -43141, 71}
         };
     Matrix<int> test_initial_vector(test_vector);
+    std::cout << test_initial_vector.ToString() << std::endl;
     return 0;
 }
--- matrix.h
+++ matrix.h
@@ -3,6 +3,9 @@
 
 #include <stdexcept>
 #include <vector>
+#include <sstream>
+#include <optional>
+#include <iomanip>
 
 /**
  * A class representing a two-dimensional mathematical matrix.
@@ -14,13 +17,14 @@ private:
     const int kSizeY;
     T** matrix_;
 
-    // TODO document this
+    // TODO document this all
     void InitialiseMatrix(T initial_value);
     void InitialiseMatrix(std::vector<std::vector<T>> initial_vector);
-    // TODO and this
     void AssertDimensions(int size_y, int size_x);
     int AssertAndGetWidth(std::vector<std::vector<T>> initial_vector);
     int AssertAndGetHeight(std::vector<std::vector<T>> initial_vector);
+    // TODO document GetLongestElemLength()
+    std::optional<T> GetLongestElemLength();
 public:
     /**
      * Initialises a new empty Matrix sized size_y * size_x filled with
@@ -40,12 +44,14 @@ public:
      */
     // I opted to use a vector as opposed to a pointer here to prevent
     // undefined behaviour when given size is larger than array's length
-    // TODO implement vector constructor
     Matrix(std::vector<std::vector<T>> initial_vector);
+
     // TODO implement copy constructor
     Matrix(const Matrix& other);
     // TODO implement move constructor
     Matrix(Matrix&& other);
+    // TODO document this
+    std::string ToString();
 };
 
 template<typename T>
@@ -82,7 +88,7 @@ void Matrix<T>::AssertDimensions(const int size_y, const int size_x) {
 }
 
 template<typename T>
-int Matrix<T>::AssertAndGetWidth(std::vector<std::vector<T>> initial_vector) {
+int Matrix<T>::AssertAndGetHeight(std::vector<std::vector<T>> initial_vector) {
     if (initial_vector.size() < 1) {
         throw std::invalid_argument("Matrix dimensions must be positive! "
                                     "Received: "
@@ -93,11 +99,11 @@ int Matrix<T>::AssertAndGetWidth(std::vector<std::vector<T>> initial_vector) {
 }
 
 template<typename T>
-int Matrix<T>::AssertAndGetHeight(std::vector<std::vector<T>> initial_vector) {
+int Matrix<T>::AssertAndGetWidth(std::vector<std::vector<T>> initial_vector) {
     // first we need to check if initial_vector[0] exists
     // technically it's pointless to call AssertAndGetWidth twice,
     // but arguments in initializer list don't have to be executed in order
-    AssertAndGetWidth(initial_vector);
+    AssertAndGetHeight(initial_vector);
 
     // compare every line's size to first_width
     // If not equal then it's not a rectangle
@@ -141,6 +147,45 @@ void Matrix<T>::InitialiseMatrix(
             matrix_[i][j] = initial_vector[i][j];
         }
     }
+}
+
+template<typename T>
+std::optional<T> Matrix<T>::GetLongestElemLength() {
+    // initial value
+    int longest_length = -1;
+    std::stringstream elemString;
+
+    for (int i = 0; i < kSizeX; i++) {
+        for (int j = 0; j < kSizeY; j++) {
+            elemString << matrix_[i][j];
+            int tmp_length = elemString.str().size();
+            if (tmp_length > longest_length) {
+                longest_length = tmp_length;
+            }
+            elemString.str(std::string());
+        }
+    }
+
+    if (longest_length > 0) {
+        return longest_length;
+    } else {
+        return {};
+    }
+}
+
+template<typename T>
+std::string Matrix<T>::ToString() {
+    std::stringstream to_string;
+    int longest_length = GetLongestElemLength().value() + 4;
+
+    for (int i = 0; i < kSizeY; i++) {
+        for (int j = 0; j < kSizeX; j++) {
+            to_string << std::right << std::setw(longest_length)
+            << matrix_[i][j];
+        }
+        to_string << '\n';
+    }
+    return to_string.str();
 }
 
 #endif //MATRIX_CPP_MATRIX_H
